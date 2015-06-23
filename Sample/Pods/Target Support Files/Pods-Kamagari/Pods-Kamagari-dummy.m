edb9314223c9589feac0df2fc0028b39a6c3b168
tasaka-kazunobu <tasaka_kazunobu@cyberagent.co.jp>
2015-06-23 14:55:30 +0900

    Add a sample project

---
+++ Sample/Pods/Target Support Files/Pods-Kamagari/Pods-Kamagari-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kamagari : NSObject
+@end
+@implementation PodsDummy_Pods_Kamagari
+@end
---
+++ Sample/Pods/Target Support Files/Pods-Kamagari/Pods-Kamagari-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double KamagariVersionNumber;
+FOUNDATION_EXPORT const unsigned char KamagariVersionString[];
+
