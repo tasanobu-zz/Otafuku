a2d21157d63aadd03d6dfd7606512cc89b6cf13c
tasaka-kazunobu <tasaka_kazunobu@cyberagent.co.jp>
2015-06-23 14:54:47 +0900

    Add Xcode project

---
+++ Source/Otafuku.h
@@ -0,0 +1,19 @@
+//
+//  Otafuku.h
+//  Otafuku
+//
+//  Created by Kazunobu Tasaka on 6/20/15.
+//  Copyright (c) 2015 Kazunobu Tasaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Otafuku.
+FOUNDATION_EXPORT double OtafukuVersionNumber;
+
+//! Project version string for Otafuku.
+FOUNDATION_EXPORT const unsigned char OtafukuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Otafuku/PublicHeader.h>
+
+
